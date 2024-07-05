fb8e9664298a1a6c75305ca50753ec8a962b72ae
Tobolich Alexander <frezar2007@gmail.com>
2024-07-05 16:14:00 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,11 @@
+Набор библиотек
+- __cleanenv__ - config
+- __slog__ - Logger
+- __sqlite3__ - storage
+- __chi__, __chi render__ - router
+
+# Cleanenv
+Установка
+```
+go get -u github.com/ilyakaznacheev/cleanenv
+```
---
+++ internal/http-server/handlers/url/delete/delete.go
@@ -0,0 +1,50 @@
+package delete
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log/slog"
+	"net/http"
+	resp "url_shortener/internal/lib/api/response"
+	"url_shortener/internal/lib/logger/sl"
+)
+
+type URLDelete interface {
+	DeleteURL(alias string) error
+}
+
+func New(log *slog.Logger, urlDelete URLDelete) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handler.url.delete.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.Status(r, 409)
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+
+		err := urlDelete.DeleteURL(alias)
+
+		if err != nil {
+			log.Error("failed to get url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("success delete", slog.String("alias", alias))
+		render.JSON(w, r, "success delete")
+	}
+}
